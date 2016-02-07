f4c8db57da52f49157a2ca2ca786930ac70dcc2e
Mohit Kishore <gkmohit@gmail.com>
2016-02-07 08:20:57 -0500

    Finished daily page 1.0

--- app/src/main/java/com/mohitkishore/guidemydough/UI/DailyPlan.java
+++ app/src/main/java/com/mohitkishore/guidemydough/UI/DailyPlan.java
@@ -3,6 +3,7 @@ package com.mohitkishore.guidemydough.UI;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.mohitkishore.guidemydough.R;
@@ -15,14 +16,17 @@ public class DailyPlan extends AppCompatActivity {
     public static int dailyBudget = 45;
 
 
-    @Bind(R.id.breakfastTextView)
-    TextView mBreakfastTextView;
-
     @Bind(R.id.budgetTextTextView)
     TextView mBudgetTextTextView;
 
     @Bind(R.id.budgetAmountTextView)
     TextView mBudgetAmountTextView;
+
+    @Bind(R.id.groceryStoreButton8)
+    Button mGroceryStoreButton8;
+
+    @Bind(R.id.dennysButtons0)
+    Button mDennysButtons0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,7 @@ public class DailyPlan extends AppCompatActivity {
         mBudgetTextTextView.setTypeface(font);
 
         mBudgetTextTextView.setText(getResources().getString(R.string.fa_cutlery) + "\n\nBreakfast Budget");
-        mBreakfastTextView.setText("Your daily total for breakfast is " + dailyBudget);
+
+        mBudgetAmountTextView.setText("$" + 14 + " of $" + dailyBudget);
     }
 }
